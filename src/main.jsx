38d2d7f706022444e5b5501a48bea16137af69e1
techking11 <techking1132000@gmail.com>
2024-04-05 14:11:45 +0630

    updated readme file

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,13 @@
-import { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.scss';
 import App from './App.jsx';
-import { BrowserRouter } from 'react-router-dom';
 import MainProvider from './provider/main.provider.jsx';
 
 const app = document.getElementById('root');
 const root = ReactDOM.createRoot(app);
 
 root.render(
-  <StrictMode>
-    <BrowserRouter>
-      <MainProvider>
-        <App />
-      </MainProvider>
-    </BrowserRouter>
-  </StrictMode>,
+  <MainProvider>
+    <App />
+  </MainProvider>
 )
--- src/provider/main.provider.jsx
+++ src/provider/main.provider.jsx
@@ -1,13 +1,19 @@
 import { UserProvider } from "../context/user.context";
 import { ProductsProvider } from "../context/product.context";
 import { CartProvider } from "../context/cart.context";
+import { StrictMode } from "react";
+import { BrowserRouter } from "react-router-dom";
 
 const MainProvider = ({ children }) => (
-  <UserProvider>
-    <ProductsProvider>
-      <CartProvider> {children} </CartProvider>
-    </ProductsProvider>
-  </UserProvider>
+  <StrictMode>
+    <BrowserRouter>
+      <UserProvider>
+        <ProductsProvider>
+          <CartProvider> {children} </CartProvider>
+        </ProductsProvider>
+      </UserProvider>
+    </BrowserRouter>
+  </StrictMode>
 )
 
 export default MainProvider;
