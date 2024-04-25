325dc6cd3154aa6d5d843848b075f2f6fb952fa2
techking11 <techking1132000@gmail.com>
2024-04-25 10:21:27 +0630

    updated persist-redux

--- src/store/store.js
+++ src/store/store.js
@@ -1,32 +1,36 @@
 import { compose, createStore, applyMiddleware } from "redux";
-import logger from 'redux-logger';
+import logger from "redux-logger";
 import { rootReducer } from "./root.reducer";
 
 import { persistStore, persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 
-// const middlewares = (store) => (next) => (action) => {
-//   if (!action.type) return next(action);
-  
-//   console.log('type: ' + action.type);
-//   console.log('payload: ' + action.payload);
-//   console.log('currentState: ' + store.getState());
-  
-//   next(action);
-//   console.log('next state: ' + store.getState());
-// };
+import { configDotenv } from "dotenv";
 
 const persistedConfig = {
-  key: 'root',
+  key: "root",
   storage,
-  blackList: ['user'],
+  blackList: ["user"],
 };
 
 const persistedReducer = persistReducer(persistedConfig, rootReducer);
 
-const middlewares = [logger];
-const enhanceMiddlewares = compose(applyMiddleware(...middlewares));
+// process.env.NODE_ENV !== "production
+const composedEnhancer =
+  (process.env.NODE_ENV !== "production" &&
+    window &&
+    window.__REDUX__DEVTOOLS__EXTENSION__COMPOSE) ||
+  compose;
 
-export const store = createStore(persistedReducer, undefined, enhanceMiddlewares);
+const middlewares = [process.env.NODE_ENV !== "production" && logger].filter(
+  Boolean
+);
+const enhanceMiddlewares = composedEnhancer(applyMiddleware(...middlewares));
+
+export const store = createStore(
+  persistedReducer,
+  undefined,
+  enhanceMiddlewares
+);
 
 export const persistor = persistStore(store);
