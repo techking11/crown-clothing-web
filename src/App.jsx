16c9e3342d23ff52ca6a73d52c179bb17b71187f
techking11 <techking1132000@gmail.com>
2024-04-27 19:58:00 +0630

    onAuthStateChanged lister to promises in redux-saga

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import Shop from "./routes/shop/shop.component";
 
 import "./app.scss";
 import Checkout from "./routes/checkout/checkout.component";
-import { createCustomUserFromAuth, onAuthStateChangeedListener } from "./utils/firebase/firebase.utils";
+import { createCustomUserFromAuth, getCurrentUser, onAuthStateChangeedListener } from "./utils/firebase/firebase.utils";
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { setCurrentUser } from "./store/user/user.action";
@@ -18,11 +18,12 @@ const App = () => {
     
     useEffect(() => {
         // const unsubscribe = 
-        onAuthStateChangeedListener((user)=> {
-            if (user) createCustomUserFromAuth(user);
-            dispatch(setCurrentUser(user));
-        });
+        // onAuthStateChangeedListener((user)=> {
+        //     if (user) createCustomUserFromAuth(user);
+        //     dispatch(setCurrentUser(user));
+        // });
         // return unsubscribe;
+        getCurrentUser().then((user) => console.log(user));
     }, []);
     
     
--- src/utils/firebase/firebase.utils.jsx
+++ src/utils/firebase/firebase.utils.jsx
@@ -106,3 +106,16 @@ export const signOutUser = async () => await signOut(auth);
 
 export const onAuthStateChangeedListener = async (callback) => 
   await onAuthStateChanged(auth, callback);
+  
+export const getCurrentUser = () => {
+  return new Promise((resolve, reject) =>{
+    const unsubscribe = onAuthStateChanged(
+      auth,
+      (userAuth) => {
+        unsubscribe();
+        resolve(userAuth);
+      },
+      // reject => console.error(reject),
+    )
+  });
+}
