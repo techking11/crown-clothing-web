37d7d4039df8faa6ee3e987a53a379b39fa953f5
techking11 <techking1132000@gmail.com>
2024-04-28 10:29:04 +0630

    user session authenticated in redux saga

--- src/store/user/user.saga.js
+++ src/store/user/user.saga.js
@@ -1,13 +1,26 @@
 import { takeLatest, call, all, put } from "redux-saga/effects";
-import { getCurrentUser } from "../../utils/firebase/firebase.utils";
+import { createCustomUserFromAuth, getCurrentUser } from "../../utils/firebase/firebase.utils";
 import { signinFailed, signinSuccess } from "./user.action";
 import { SET_ACTIONS_TYPE } from "./user.type";
+
+export function* getSnapshotFromAuth(userAuth, additionalDetails) {
+  try {
+    const userSnapshot = yield call(
+      createCustomUserFromAuth,
+      userAuth,
+      additionalDetails
+    );
+    yield put(signinSuccess({id: userSnapshot.id, ...userSnapshot.data()}))
+  } catch (error) {
+    yield put(signinFailed(error))
+  }
+}
 
 export function* isUserAuthenticated() {
   try {
     const userAuth = yield call(getCurrentUser);
     if(!userAuth) return;
-    yield put(signinSuccess(userAuth));
+    yield put(getSnapshotFromAuth, userAuth);
   } catch (error) {
     yield put(signinFailed(error))
   }
